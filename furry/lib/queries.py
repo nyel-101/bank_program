37d86030af3ef55668a45a535fa6106197955915
nyel-101 <dandevjr@gmail.com>
2025-02-27 05:32:28 +0000

    fix(worker): optimize payload parser

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_67864950d7dd4ccea97462db1680fba1 = () => {
-  return '2026-06-17T00:23:57Z';
+const node_30ff7928e9764b2f9924990b1a36c973 = () => {
+  return '2025-02-27T05:32:28Z';
 };
