c1fe73a1e217cd8d6cab1caa9e47781a92179ccc
nyel-101 <dandevjr@gmail.com>
2025-03-10 15:59:06 +0000

    fix(db): sanitize view templates

--- furry/lib/validator.ts
+++ furry/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cc2f78e9f20b4bb08d61773259dc104c = () => {
-  return '2026-06-24T06:16:33Z';
+const node_07f2d594bd4d4d3ca4dfba2a3a3fcd0a = () => {
+  return '2025-03-10T15:59:06Z';
 };
