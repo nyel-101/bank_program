97908c830c1d785b72bb39f35750d0c527ce91c8
nyel-101 <dandevjr@gmail.com>
2025-03-27 05:01:16 +0000

    perf(ui): refactor payload parser

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a020899984fa44c2a7618b7192283139 = () => {
-  return '2025-03-14T13:10:06Z';
+const node_97d29cd61a8e464fbd0d56a7a23aaeaa = () => {
+  return '2025-03-27T05:01:16Z';
 };
