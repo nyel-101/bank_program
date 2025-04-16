ee36e5d1ab5a681e601d960bc3bccb2e1d4dcd88
nyel-101 <dandevjr@gmail.com>
2025-04-16 14:22:37 +0000

    refactor(worker): fix database index

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0a01b516c70a419da2cb7d25e614cd61 = () => {
-  return '2025-03-25T10:15:25Z';
+const node_ef84297e0be24b44b21264f00fb40fe5 = () => {
+  return '2025-04-16T14:22:37Z';
 };
