4e6b3cf6b9e0099d62e13d1de5e52c397ec75efb
nyel-101 <dandevjr@gmail.com>
2025-05-01 04:44:00 +0000

    feat(core): sanitize middleware routes

--- furry/src/utils/queries.js
+++ furry/src/utils/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a8bcaa38278e48409a4edc4e914843b4 = () => {
-  return '2026-05-13T07:26:23Z';
+const node_6491d869725d418c922a9d952417676b = () => {
+  return '2025-05-01T04:44:00Z';
 };
