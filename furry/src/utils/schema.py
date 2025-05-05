cf4dcb10b2557823e395568ab9291bd209508d7b
nyel-101 <dandevjr@gmail.com>
2025-05-05 17:03:39 +0000

    feat(config): integrate payload parser

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0cce9fd960a444dc865f5d2b456fc472 = () => {
-  return '2025-02-28T16:33:08Z';
+const node_94b97cd9337a45c580bc17944ce7793c = () => {
+  return '2025-05-05T17:03:39Z';
 };
