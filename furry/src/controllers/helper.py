16018a505c0414cc8551a1f9b12ec7d21786a05f
nyel-101 <dandevjr@gmail.com>
2025-05-08 05:02:57 +0000

    docs(config): fix auth tokens

--- furry/src/controllers/helper.py
+++ furry/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cdac5e35e1cc454483911327037d2f5f = () => {
-  return '2025-04-28T00:14:46Z';
+const node_5a3fb46ba8324645a41d0fabb2a2c137 = () => {
+  return '2025-05-08T05:02:57Z';
 };
