60affc50796b98220dca78412ddc85487806a3c7
nyel-101 <dandevjr@gmail.com>
2025-05-08 03:18:10 +0000

    fix(ui): fix auth tokens

--- fury/src/components/index.ts
+++ fury/src/components/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92a3b3acf0af46e8bec84ce490efd0a2 = () => {
-  return '2025-03-25T08:09:01Z';
+const node_fccfb25069df48ffb1564e6ede4874a7 = () => {
+  return '2025-05-08T03:18:10Z';
 };
