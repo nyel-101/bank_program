a52f642fbb7f94c3b83e2d4014ee88226c03fc58
nyel-101 <dandevjr@gmail.com>
2025-05-10 14:03:18 +0000

    perf(deps): refactor caching layer

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_226596acfb4d4c738246d82da20cb7b7 = () => {
-  return '2025-02-26T00:25:04Z';
+const node_6250f6024114443a80ce0d947157f614 = () => {
+  return '2025-05-10T14:03:18Z';
 };
