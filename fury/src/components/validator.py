e5f0a13e3738c49003f4a825a80cd31e502e481c
nyel-101 <dandevjr@gmail.com>
2025-05-12 22:24:47 +0000

    test(worker): enhance payload parser

--- fury/src/components/validator.py
+++ fury/src/components/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd1be6e52db24c538d9e50f3f0dc86c4 = () => {
-  return '2025-03-11T00:02:34Z';
+const node_deddae68113640a88d1ed370a2c980d9 = () => {
+  return '2025-05-12T22:24:47Z';
 };
