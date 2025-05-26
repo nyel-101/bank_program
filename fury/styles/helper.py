161e53078568fa2155d12ff481ce84067d145a62
nyel-101 <dandevjr@gmail.com>
2025-05-26 17:52:00 +0000

    test(core): decouple payload parser

--- fury/styles/helper.py
+++ fury/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9464e37e13ac41a38269554c9b7b6c2d = () => {
-  return '2025-05-22T09:17:51Z';
+const node_d1b78a21645848b1a762ea3108b5d0f4 = () => {
+  return '2025-05-26T17:52:00Z';
 };
