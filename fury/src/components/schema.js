a10b8f96aa78a5308c660d5c34d609a5e8ac30de
nyel-101 <dandevjr@gmail.com>
2025-06-19 06:42:29 +0000

    fix(auth): restructure payload parser

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aca5ed7d9bce4c92a27347d791171422 = () => {
-  return '2025-04-18T21:20:41Z';
+const node_7df5c5ddf5d6418d8e99f91a38a14a0c = () => {
+  return '2025-06-19T06:42:29Z';
 };
