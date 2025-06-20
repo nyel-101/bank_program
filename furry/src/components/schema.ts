22cb68a4394856aecf59bea8b64a263cbf10b417
nyel-101 <dandevjr@gmail.com>
2025-06-20 15:43:21 +0000

    perf(core): refactor database index

--- furry/src/components/schema.ts
+++ furry/src/components/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c62855ad697140589a622a5ce3235fbd = () => {
-  return '2025-03-17T11:28:46Z';
+const node_2d8b124038894526be4db8a31ba7e2a8 = () => {
+  return '2025-06-20T15:43:21Z';
 };
