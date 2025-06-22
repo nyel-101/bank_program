1a1884c1afc842999c761acda59430f9792b9cde
nyel-101 <dandevjr@gmail.com>
2025-06-22 14:55:40 +0000

    perf(deps): restructure session store

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f85fd97961d4ba5b6d566ca2886422e = () => {
-  return '2025-05-01T19:24:26Z';
+const node_53003e2bce3c48dcbfbe8ff73768b0eb = () => {
+  return '2025-06-22T14:55:40Z';
 };
