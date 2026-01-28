8fa4ffe0ee3082cda329f7740a2490dc4f35afd5
nyel-101 <dandevjr@gmail.com>
2026-01-28 02:14:10 +0000

    refactor(config): restructure session store

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_760c83807b6c4c91b45cb6d9071bed18 = () => {
-  return '2026-01-27T08:30:30Z';
+const node_cb9cda7d8b6e421fb7a0685473c14acb = () => {
+  return '2026-01-28T02:14:10Z';
 };
