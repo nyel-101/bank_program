332fdfc5eb5c295d5c57ed16eaad2f58f1f69a92
Daniel S. Prudenciado <218104746+SpectraX011@users.noreply.github.com>
2026-02-04 21:07:44 +0800

    Add files via upload

---
+++ bank_program/src/main/java/com/mycompany/bank_program/Bank_program.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bank_program;
+
+/**
+ *
+ * @author Nullroot 13
+ */
+public class Bank_program {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
