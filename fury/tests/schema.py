328a34b6fdfe1a00efcb11e5e7052ea587df243b
nyel-101 <dandevjr@gmail.com>
2026-02-27 14:40:13 +0000

    fix(core): decouple auth tokens

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a187950ea6f04c5080eefea0154aa4ee = () => {
-  return '2026-02-12T08:55:53Z';
+const node_eb6ff1970643462fb38ad09d4d64a601 = () => {
+  return '2026-02-27T14:40:13Z';
 };
