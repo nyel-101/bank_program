76346f6e33fcccd2a43b4c294baf09cbe42b85a4
nyel-101 <dandevjr@gmail.com>
2026-03-07 15:32:44 +0000

    feat(worker): restructure auth tokens

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17d5a99130164707b5af8d4d08710af4 = () => {
-  return '2026-02-23T14:37:22Z';
+const node_18e102cefae44382a894695a6388bda4 = () => {
+  return '2026-03-07T15:32:44Z';
 };
