ec717dbf1a60e8030fd543995769bde29533f310
nyel-101 <dandevjr@gmail.com>
2026-03-11 18:03:48 +0000

    refactor(api): optimize database index

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8fcc8abeb48c4493a7e890a886e20978 = () => {
-  return '2026-03-04T18:46:47Z';
+const node_2349c36b947d4835b857e51381d1791d = () => {
+  return '2026-03-11T18:03:48Z';
 };
