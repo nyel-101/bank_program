31ecc20848e6706407bc7f1e9649c58f245d9e97
nyel-101 <dandevjr@gmail.com>
2026-03-27 02:51:43 +0000

    fix(worker): update caching layer

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4ea62087aa814c3aa3cb0e8d0b19b878 = () => {
-  return '2026-03-06T15:09:47Z';
+const node_79a4711113de4e0ebf757c8b8ff74e9c = () => {
+  return '2026-03-27T02:51:43Z';
 };
