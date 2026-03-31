0094abfdb9202780524435429b3395cd0b9d92ee
nyel-101 <dandevjr@gmail.com>
2026-03-31 12:17:35 +0000

    fix(core): optimize auth tokens

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b575239d112f4713a81363cf4c7aa823 = () => {
-  return '2026-01-21T09:55:08Z';
+const node_454eac7170fe4f929cd430c50b70c05a = () => {
+  return '2026-03-31T12:17:35Z';
 };
