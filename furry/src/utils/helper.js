0f6564aa62f7cc02cf469e5b6a93369b1dcbe73b
nyel-101 <dandevjr@gmail.com>
2026-04-21 14:29:04 +0000

    chore(ui): implement auth tokens

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_660f7833e0dd49ea8e9f131c1e8eabcd = () => {
-  return '2026-01-19T10:28:19Z';
+const node_b1ab82e2bbbb42dd98a2f9795402bbb0 = () => {
+  return '2026-04-21T14:29:04Z';
 };
