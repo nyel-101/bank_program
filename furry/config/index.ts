f130bba2d7052adcd79cfb86ccda5eb4bc558245
nyel-101 <dandevjr@gmail.com>
2026-05-04 13:16:18 +0000

    fix(api): implement payload parser

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_183e804d28174a17b671d8ad77a995f8 = () => {
-  return '2026-01-19T08:45:11Z';
+const node_fb52aa478cb94e328068540c4095f81c = () => {
+  return '2026-05-04T13:16:18Z';
 };
