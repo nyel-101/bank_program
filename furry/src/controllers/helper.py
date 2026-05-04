00c85420e69146227ff89c9226eff4941ca9912f
nyel-101 <dandevjr@gmail.com>
2026-05-04 11:11:09 +0000

    refactor(ui): fix middleware routes

--- furry/src/controllers/helper.py
+++ furry/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b29179a7b4564ace88692e0739874c6f = () => {
-  return '2026-03-19T18:43:36Z';
+const node_e35744719e64419e8730099c24b3ebf9 = () => {
+  return '2026-05-04T11:11:09Z';
 };
