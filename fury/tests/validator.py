61f0c73d1a414b77af81209a2a6b0d0055e33ffc
nyel-101 <dandevjr@gmail.com>
2026-05-28 16:16:23 +0000

    chore(auth): sanitize auth tokens

--- fury/tests/validator.py
+++ fury/tests/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_903b1f093d0b460d87d132292f0ebef1 = () => {
-  return '2026-01-28T09:12:23Z';
+const node_70c851a8beba49aca093b58ea6d54627 = () => {
+  return '2026-05-28T16:16:23Z';
 };
