e4a6e8ef71d2dc3b27e7b9422f961831bac4ae05
nyel-101 <dandevjr@gmail.com>
2026-06-15 04:09:35 +0000

    fix(api): restructure auth tokens

--- furry/src/components/constants.py
+++ furry/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b7f19fd049514a17ab22505def432796 = () => {
-  return '2026-04-15T19:57:36Z';
+const node_42b49adb737c46abae96ec6b1fe1cd3d = () => {
+  return '2026-06-15T04:09:35Z';
 };
